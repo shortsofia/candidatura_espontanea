77952483cca487f271c13fd989ef02744fe0cc1f
Cláudia Silva <ditinha_2@hotmail.com>
2016-11-02 16:12:09 +0000

    Add files via upload

---
+++ candidatura_espontanea_novatentativa/Assets/candidatura_espontanea_novatentativa_edge.js
@@ -0,0 +1,77 @@
+
+(function(compId){var _=null,y=true,n=false,x105='149px',x97='seta_cor',x101='PORTEFOLIO_botao',x38='502px',e88='${comp_enter}',x103='-83px',e85='${comp}',x22='257px',x116='32px',x52='42px',x79='-180',e10='${portefolio2}',x126='-31px',d='display',i='none',x94='-30px',x59='512px',x13='1002px',x39='612px',e9='${curriculo2}',e86='${Texto_comp}',x5='rgba(255,255,255,1)',x136='600px',e90='${seta_cor3}',x82='seta_cor3',e123='${PORTEFOLIO_botao}',x1='6.0.0',e122='${seta_cor2}',x2='5.0.0',c='color',x98='696px',x131='contactos_botao',x47='italic',x4='both',x53='comp_enter',x70='416px',x140='seta_cor4',x67='30px',x83='0',x100='-90',e142='${seta_brancaCopy}',x139='18px',x19='1024px',p='px',x26='pointer',x66='Texto_ferr',x99='seta_cor2',x111='52px',x58='Texto_comp',x77='33px',x35='curriculo',x106='29px',x56='ferr_enter',e34='${botao_lado}',b='block',x114='51px',x81='0.80952',x15='IMG_8944',x23='478px',x92='portefolio',x45='rgba(211,30,52,1.00)',x133='rgba(255,255,255,1.00)',x109='384px',x127='697px',x51='185px',e33='${botao_baixo}',x3='6.0.0.400',x49='485px',lf='left',x18='inicio',x110='1018px',x115='41px',x78='21px',x96='11px',tp='top',x50='187px',x46='300',x124='contactos',e143='${seta_branca}',e141='${contactos_botao}',x138='40px',x36='768px',x129='523px',x61='423px',x117='17px',x16='rgba(0,0,0,0)',x135='-130px',x134='700',g='image',po='center',x130='seta_brancaCopy',x128='seta_branca',e121='${seta_cor}',x119='0.5',x108='6px',x118='seta_branca2',x48='break-word',x112='link',x='text',x63='open-sans, sans-serif',x104='592px',x12='1503px',m='rect',x11='0px',x20='1536px',x30='botao_lado',x40='378px',x64='rgba(82,82,82,1.00)',x28='230px',x75='comp',x69='259px',x41='26px',e6='${inicio2}',e91='${ferr_enter}',x60='244px',x25='botao_baixo',x95='515px',x14='auto',x137='44px',x24='54px',x107='rgba(211,30,52,1)',e87='${Texto_ferr}',x44='Arial, Helvetica, sans-serif',x73='ferr',l='normal',o='opacity',e89='${ferr}',x62='173px',x55='695px',x42='Text',x29='1302px',w='width',x80='1',x32='true',e8='${contactos2}',e7='${imagem_fundo}',x71='127px',x68='519px';var g74='exports_candidaturaespontanea-20.png',g27='exports_candidaturaespontanea-14.png',g57='exports_candidaturaespontanea-19.png',g37='curriculo.png',g54='exports_candidaturaespontanea-18.png',g93='portefolio.png',g17='IMG_8944.png',g125='contactos.png',g31='exports_candidaturaespontanea-15.png',g76='exports_candidaturaespontanea-21.png',g84='seta_cor.png',g120='seta_branca.png',g21='inicio.png';var s65="<p style=\"margin: 0px;\">​- Captação, edição e pós-produção de vídeo, áudio e fotografia.</p><p style=\"margin: 0px;\">- Web design e design gráfico.</p><p style=\"margin: 0px;\">- Animação de imagens 2D e 3D;</p><p style=\"margin: 0px;\">- Modelação 3D;</p><p style=\"margin: 0px;\">- Concepção, produção, e edição de conteúdos multimédia para plataformas online e offline.</p>",s43="<p style=\"margin: 0px;\">​https://pt.linkedin.com/in/cláudia-silva-a990a78a</p>",s102="<p style=\"margin: 0px;\">​<span style=\"text-transform: uppercase; font-weight: 700;\">Portefólio</span></p>",s132="<p style=\"margin: 0px;\">​<span style=\"text-transform: uppercase;\">Contactos</span><span style=\"text-transform: uppercase; font-style: normal;\">​</span><span style=\"text-transform: uppercase; font-style: normal; font-weight: 700;\">​</span></p>",s113="<p style=\"margin: 0px;\"><span style=\"color: rgb(255, 255, 255);\">www.behance.com/Claudia_Silva​</span></p>",s72="<p style=\"margin: 0px; line-height: 35px;\">​Photoshop Illustrator InDesign</p><p style=\"margin: 0px; line-height: 35px;\">After<span style=\"word-spacing: 0px;\"> </span>Effects Premiere Edge<span style=\"word-spacing: 0px;\"> </span>Animate</p><p style=\"margin: 0px; line-height: 35px;\">Dreamweaver Avid Blender</p><p style=\"margin: 0px;\">​</p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'open-sans, sans-serif':'<script src=\"http://use.edgefonts.net/open-sans:n7,i7,n8,i8,i4,n3,i3,n4,n6,i6:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x4,rI:n,cn:{dom:[{id:'imagem_fundo',symbolName:'imagem_fundo',t:m,r:['-450px','-41','1503','1002','auto','auto']},{id:'inicio2',symbolName:'inicio',t:m,r:['-1024px','0px','1024','1536','auto','auto']},{id:'curriculo2',symbolName:'curriculo',t:m,r:['1024px','0px','1024','768','auto','auto']},{id:'portefolio2',symbolName:'portefolio',t:m,r:['1077px','0','1024','768','auto','auto']},{id:'contactos2',symbolName:'contactos',t:m,r:['0px','0px','1024','768','auto','auto']}],style:{'${Stage}':{isStage:true,r:['null','null','1024px','768px','auto','auto'],overflow:'hidden',f:[x5]}}},tt:{d:7500,a:y,data:[["eid4",tp,2000,1000,"linear",e6,'0px','-768px'],["eid49",lf,3500,1000,"linear",e7,'-2px','-450px'],["eid19",lf,5674,326,"linear",e8,'1074px','1004px'],["eid13",lf,6500,1000,"linear",e8,'1004px','0px'],["eid6",lf,3500,1000,"linear",e6,'0px','-1024px'],["eid8",lf,3500,1000,"linear",e9,'1024px','0px'],["eid100",tp,5444,2056,"linear",e8,'-1px','0px'],["eid16",lf,4282,218,"linear",e10,'1077px','1004px'],["eid11",lf,5000,1000,"linear",e10,'1004px','0px']]}},"imagem_fundo":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x11,x11,x12,x13,x14,x14],id:x15,t:g,f:[x16,im+g17,x11,x11]}],style:{'${symbolSelector}':{r:[_,_,x12,x13]}}},tt:{d:0,a:y,data:[]}},"inicio":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x18,t:g,r:[x11,x11,x19,x20,x14,x14],f:[x16,im+g21,x11,x11]},{r:[x22,x23,x24,x24,x14,x14],id:x25,t:g,cu:x26,f:[x16,im+g27,x11,x11]},{r:[x28,x29,x24,x24,x14,x14],id:x30,t:g,cu:x26,f:[x16,im+g31,x11,x11]}],style:{'${symbolSelector}':{isStage:x32,r:[undefined,undefined,x19,x20]}}},tt:{d:1191,a:y,data:[["eid25",tp,0,335,"linear",e33,'492px','511px'],["eid26",tp,335,415,"linear",e33,'511px','480px'],["eid37",tp,750,441,"linear",e33,'480px','492px'],["eid45",lf,0,335,"linear",e34,'230px','250px'],["eid46",lf,335,415,"easeInOutQuad",e34,'250px','220px'],["eid47",lf,750,441,"easeInOutQuad",e34,'220px','230px']]}},"curriculo":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:g,id:x35,r:[x11,x11,x19,x36,x14,x14],f:[x16,im+g37,x11,x11]},{t:x,r:[x38,x39,x40,x41,x14,x14],id:x42,text:s43,cu:x26,n:[x44,[14,p],x45,x46,i,x47,x48,l]},{r:[x49,x50,x51,x52,x14,x14],t:g,id:x53,v:b,cu:x26,f:[x16,im+g54,x11,x11]},{r:[x55,x50,x51,x52,x14,x14],t:g,id:x56,v:i,cu:x26,f:[x16,im+g57,x11,x11]},{t:x,id:x58,ts:['','','','',i],r:[x59,x60,x61,x62,x14,x14],v:b,align:lf,n:[x63,[17,p],x64,x46,i,l,x48,l],text:s65},{t:x,id:x66,ts:['',x67,'','',i],r:[x68,x69,x70,x71,x14,x14],v:i,align:lf,n:[x63,[17,p],x64,x46,i,l,x48,l],text:s72},{r:[x55,x50,x51,x52,x14,x14],t:g,id:x73,v:b,cu:x26,f:[x16,im+g74,x11,x11]},{r:[x49,x50,x51,x52,x14,x14],t:g,id:x75,v:i,cu:x26,f:[x16,im+g76,x11,x11]},{t:g,r:[x52,x77,x67,x78,x14,x14],tf:[[],[x79],[],[x80,x81]],id:x82,o:x83,cu:x26,f:[x16,im+g84,x11,x11]}],style:{'${symbolSelector}':{r:[_,_,x19,x36]}}},tt:{d:1500,a:y,data:[["eid106",d,0,0,"linear",e85,i,i],["eid108",c,0,0,"linear",e86,'rgba(82,82,82,1.00)','rgba(82,82,82,1.00)'],["eid53",lf,0,0,"linear",e86,'512px','512px'],["eid54",w,0,0,"linear",e86,'423px','423px'],["eid52",d,0,0,"linear",e87,i,i],["eid68",d,0,0,"linear",e88,b,b],["eid107",d,0,0,"linear",e89,b,b],["eid51",d,0,0,"linear",e86,b,b],["eid109",c,0,0,"linear",e87,'rgba(82,82,82,1.00)','rgba(82,82,82,1.00)'],["eid105",o,0,0,"linear",e90,'0','0'],["eid102",o,750,750,"linear",e90,'0','0.5'],["eid67",d,0,0,"linear",e91,i,i]]}},"portefolio":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x11,x11,x19,x36,x14,x14],id:x92,t:g,f:[x16,im+g93,x11,x11]},{r:[x94,x95,x67,x96,x14,x14],id:x97,t:g,f:[x16,im+g84,x11,x11]},{r:[x94,x98,x67,x96,x14,x14],id:x99,t:g,f:[x16,im+g84,x11,x11]},{tf:[[],[x100],[0,0,0],[1,1,1]],id:x101,text:s102,cu:x26,n:[x63,[22,p],x45,x46,i,l,x48,l],align:lf,r:[x103,x104,x105,x106,x14,x14],t:x,ts:['','','','',i]},{t:x,align:po,ts:['','','','',i],cu:x26,n:[x63,[40,p],x107,x46,i,x47,x48,l],r:[x108,x109,x110,x111,x14,x14],id:x112,text:s113},{tf:[[],[x79],[0,0,0],[1,1,1]],t:g,r:[x114,x115,x116,x117,x14,x14],id:x118,o:x119,cu:x26,f:[x16,im+g120,x11,x11]}],style:{'${symbolSelector}':{r:[_,_,x19,x36]}}},tt:{d:1237,a:y,data:[["eid57",lf,0,510,"linear",e121,'-30px','-40px'],["eid60",lf,510,727,"linear",e121,'-40px','-30px'],["eid58",lf,0,510,"linear",e122,'-30px','-40px'],["eid59",lf,510,727,"linear",e122,'-40px','-30px'],["eid62",lf,0,510,"linear",e123,'-83px','-95px'],["eid64",lf,510,727,"linear",e123,'-95px','-83px']]}},"contactos":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x11,x11,x19,x36,x14,x14],id:x124,t:g,f:[x16,im+g125,x11,x11]},{r:[x126,x127,x67,x96,x14,x14],id:x128,t:g,f:[x16,im+g120,x11,x11]},{r:[x126,x129,x67,x96,x14,x14],id:x130,t:g,f:[x16,im+g120,x11,x11]},{t:x,id:x131,text:s132,cu:x26,n:[x63,[22,p],x133,x134,i,l,x48,l],align:po,r:[x135,x136,x60,x77,x14,x14],tf:[[],[x100],[0,0,0],[1,1,1]],ts:['','','','',i]},{r:[x137,x138,x67,x139,x14,x14],tf:[[],[x79],[0,0,0],[1,1,1]],t:g,id:x140,o:x119,cu:x26,f:[x16,im+g84,x11,x11]}],style:{'${symbolSelector}':{r:[_,_,x19,x36]}}},tt:{d:1000,a:y,data:[["eid97",lf,0,492,"linear",e141,'-130px','-143px'],["eid98",lf,492,508,"linear",e141,'-143px','-130px'],["eid72",lf,0,492,"linear",e142,'-31px','-44px'],["eid79",lf,492,508,"linear",e142,'-44px','-31px'],["eid73",lf,0,492,"linear",e143,'-31px','-44px'],["eid80",lf,492,508,"linear",e143,'-44px','-31px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-1320203");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1915,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",7500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'imagem_fundo'
+(function(symbolName){})("imagem_fundo");
+//Edge symbol end:'imagem_fundo'
+
+//=========================================================
+
+//Edge symbol: 'inicio'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1191,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${botao_baixo}","click",function(sym,e){sym.getComposition().getStage().play(2000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${botao_lado}","click",function(sym,e){sym.getComposition().getStage().play(3500);sym.getComposition().getStage().getSymbol("curriculo2").play(0);});
+//Edge binding end
+})("inicio");
+//Edge symbol end:'inicio'
+
+//=========================================================
+
+//Edge symbol: 'curriculo'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Text}","click",function(sym,e){window.open("https://pt.linkedin.com/in/cláudia-silva-a990a78a","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${comp}","click",function(sym,e){sym.$("comp").hide();sym.$("comp_enter").show();sym.$("Texto_comp").show();sym.$("Texto_ferr").hide();sym.$("ferr_enter").hide();sym.$("ferr").show();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ferr}","click",function(sym,e){sym.$("ferr").hide();sym.$("ferr_enter").show();sym.$("Texto_ferr").show();sym.$("Texto_comp").hide();sym.$("comp_enter").hide();sym.$("comp").show();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${seta_cor3}","click",function(sym,e){sym.getComposition().getStage().playReverse(4500);sym.playReverse(1250);});
+//Edge binding end
+})("curriculo");
+//Edge symbol end:'curriculo'
+
+//=========================================================
+
+//Edge symbol: 'portefolio'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1237,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${PORTEFOLIO_botao}","click",function(sym,e){sym.getComposition().getStage().play(5000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${link}","click",function(sym,e){window.open("http://www.behance.com/Claudia_Silva","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${seta_branca2}","click",function(sym,e){sym.getComposition().getStage().playReverse(6000);});
+//Edge binding end
+})("portefolio");
+//Edge symbol end:'portefolio'
+
+//=========================================================
+
+//Edge symbol: 'contactos'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${contactos_botao}","click",function(sym,e){sym.getComposition().getStage().play(6500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${seta_cor4}","click",function(sym,e){sym.getComposition().getStage().playReverse(7500);});
+//Edge binding end
+})("contactos");
+//Edge symbol end:'contactos'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-1320203");
